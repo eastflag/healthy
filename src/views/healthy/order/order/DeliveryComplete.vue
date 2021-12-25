c1e5785f89397933416fa708f8608a7bb538477a
qkrwlgh91 <qkrwlgh91@gmail.com>
2021-12-26 01:08:35 +0900

    임시커밋

--- src/views/healthy/order/order/DeliveryComplete.vue
+++ src/views/healthy/order/order/DeliveryComplete.vue
@@ -1,17 +1,25 @@
 <template xmlns:v-slot="http://www.w3.org/1999/XSL/Transform">
     <v-container fluid class="containerLayout">
         <template>
+            <div class="content">
+                <div class="menuNav">
+                    <span>주문관리</span><v-icon class="pagingArrow">$pagingArrow</v-icon><span>주문관리</span><v-icon class="actionPagingArrow">$actionPagingArrow</v-icon><span>배송완료</span>
+                </div>
+                <div class="title">배송완료</div>
+            </div>
+        </template>
+        <template>
+            <delivering-complete-search-input />
+        </template>
+        <template>
+            <delivering-complete-list />
+        </template>
+        <template>
             <div class="button d-flex align-center justify-end">
                 <basic-btn>히스토리</basic-btn>
                 <basic-btn>목록</basic-btn>
                 <action-btn>전체주문취소</action-btn>
             </div>
-        </template>
-        <template>
-            <!--컨포넌트-->
-        </template>
-        <template>
-            <!--컨포넌트 추가 생성 ++-->
         </template>
     </v-container>
 </template>
@@ -19,9 +27,12 @@
 <script>
     import BasicBtn from '@/components/common/buttonComponents/BasicBtn'
     import ActionBtn from '@/components/common/buttonComponents/ActionBtn'
+    import deliveryCompleteSearchInput from '@/components/common/eachPageComponents/orderComponents/DeliveryCompleteSearchInput'
+    import deliveryCompleteList from '@/components/common/eachPageComponents/orderComponents/DeliveryCompleteList'
+
     export default {
         name: "DeliveryComplete",
-        components:{ BasicBtn , ActionBtn }
+        components:{ BasicBtn , ActionBtn, deliveryCompleteSearchInput, deliveryCompleteList }
     }
 </script>
 
