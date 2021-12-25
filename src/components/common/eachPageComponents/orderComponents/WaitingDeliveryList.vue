a567cbbb147229abaa1c30880e83c5b569efe325
qkrwlgh91 <qkrwlgh91@gmail.com>
2021-12-25 22:59:39 +0900

    배송대기 목록

---
+++ src/components/common/eachPageComponents/orderComponents/WaitingDeliveryList.vue
@@ -0,0 +1,143 @@
+<template>
+    <v-container fluid class="boardLayout">
+        <div class="title d-flex justify-space-between">
+            <div class="d-flex"><span>LIST</span> ('{{num}}'의 리스트가 조회되었습니다.)</div>
+            <div class="selectbx">
+                <select id="departure" name="departure">
+                    <option value="num1" selected>주문일 최신 순</option>
+                    <option value="num2">주문일 과거 순</option>
+                </select>
+                <basic-btn class="ml-3">엑셀다운</basic-btn>
+                <basic-btn class="ml-3">운송장일괄등록</basic-btn>
+                <basic-btn class="ml-3">일괄수정</basic-btn>
+            </div>
+        </div>
+        <table>
+            <colgroup>
+                <col width="4%">
+                <col width="15%">
+                <col width="10%">
+                <col width="12%">
+                <col width="10%">
+                <col width="18%">
+                <col width="6%">
+                <col width="10%">
+                <col width="10%">
+                <col width="7%">
+            </colgroup>
+            <thead>
+                <tr>
+                    <th>
+                        <div class="spot">
+                            <input class="checkbx" type="checkbox" id="list1" name="list_check"><label for="list1"></label>
+                        </div>
+                    </th>
+                    <th><span>주문번호</span></th>
+                    <th><span>상태</span></th>
+                    <th><span>주문일<br/>(결제일)</span></th>
+                    <th><span>스토어</span></th>
+                    <th><span>제품명</span></th>
+                    <th><span>주문수량</span></th>
+                    <th><span>주문금액</span></th>
+                    <th><span>배송구분</span></th>
+                    <th><span>비고</span></th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>
+                        <div class="spot">
+                            <input class="checkbx" type="checkbox" id="list2" name="list_check"><label for="list2"></label>
+                        </div>
+                    </td>
+                    <td><span><a>OD202111001-123456</a></span></td>
+                    <td><span>주문접수</span></td>
+                    <td><span>2022-09-01 09:46:26<br/>(2021-09-01 09:47:27)</span></td>
+                    <td><span>종근당</span></td>
+                    <td><span>종근당활력 홍삼파워 10글자 이상테스트</span></td>
+                    <td><span>1</span></td>
+                    <td><span>70,000</span></td>
+                    <td><span></span></td>
+                    <td><span></span></td>
+                </tr>
+                <tr>
+                    <td>
+                        <div class="spot">
+                            <input class="checkbx" type="checkbox" id="list3" name="list_check"><label for="list3"></label>
+                        </div>
+                    </td>
+                    <td><span><a>OD202111001-123456</a></span></td>
+                    <td><span>주문접수</span></td>
+                    <td><span>2022-09-01 09:46:26<br/>(2021-09-01 09:47:27)</span></td>
+                    <td><span>종근당</span></td>
+                    <td><span>종근당활력 홍삼파워 10글자 이상테스트</span></td>
+                    <td><span>1</span></td>
+                    <td><span>70,000</span></td>
+                    <td><span></span></td>
+                    <td><span></span></td>
+                </tr>
+            </tbody>
+        </table>
+        <plus-btn>더보기</plus-btn>
+    </v-container>
+</template>
+
+<script>
+    import PlusBtn from '../../buttonComponents/PlusBtn'
+    import BasicBtn from '../../buttonComponents/BasicBtn'
+
+    export default {
+        name: "OrderList",
+        components: { PlusBtn, BasicBtn },
+        data(){
+            return{
+                num:'2'
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    @import '~@/assets/scss/_index.scss';
+    // ListForm Common SCSS
+    @import '~@/assets/scss/listFormCommon.scss';
+
+    #app{
+        .boardLayout {
+            min-height: 675px;
+        }
+        table {
+            tbody {
+                td { 
+                    &:nth-child(2) {
+                        span {
+                            a {
+                                text-decoration: underline;
+                                color: $color-basic;
+                            }
+                        }
+                    }
+                    &:nth-child(6) {
+                        height: inherit;
+                        justify-content: center;
+                        align-items: center;
+                        text-align: center;
+                        span {
+                            display: inline-block;
+                            width: 140px;
+                            overflow: hidden;
+                            text-overflow: ellipsis;
+                            white-space: nowrap;
+                            margin-bottom: -7px;
+                        }
+                    }
+                    &:nth-child(9), 
+                    &:nth-child(10) {
+                        height: inherit;
+                    }
+                }
+            }
+        }
+    }
+</style>
+
--- src/views/healthy/order/order/WaitingDelivery.vue
+++ src/views/healthy/order/order/WaitingDelivery.vue
@@ -1,16 +1,24 @@
 <template xmlns:v-slot="http://www.w3.org/1999/XSL/Transform">
     <v-container fluid class="containerLayout">
         <template>
+            <div class="content">
+                <div class="menuNav">
+                    <span>주문관리</span><v-icon class="pagingArrow">$pagingArrow</v-icon><span>주문관리</span><v-icon class="actionPagingArrow">$actionPagingArrow</v-icon><span>배송대기</span>
+                </div>
+                <div class="title">배송대기</div>
+            </div>
+        </template>
+        <template>
+            <waiting-delivery-search-input />
+        </template>
+        <template>
+            <waiting-delivery-list />
+        </template>
+        <template>
             <div class="button d-flex align-center justify-end">
                 <basic-btn>목록</basic-btn>
                 <action-btn>전체주문취소</action-btn>
             </div>
-        </template>
-        <template>
-            <!--컨포넌트-->
-        </template>
-        <template>
-            <!--컨포넌트-->
         </template>
     </v-container>
 </template>
@@ -18,9 +26,12 @@
 <script>
     import BasicBtn from '@/components/common/buttonComponents/BasicBtn'
     import ActionBtn from '@/components/common/buttonComponents/ActionBtn'
+    import waitingDeliverySearchInput from '@/components/common/eachPageComponents/orderComponents/WaitingDeliverySearchInput'
+    import waitingDeliveryList from '@/components/common/eachPageComponents/orderComponents/WaitingDeliveryList'
+
     export default {
         name: "WaitingDelivery",
-        components:{ BasicBtn , ActionBtn }
+        components:{ BasicBtn , ActionBtn, waitingDeliverySearchInput, waitingDeliveryList }
     }
 </script>
 
