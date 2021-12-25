76583af3218fce94400a32af3b935a86477c7865
qkrwlgh91 <qkrwlgh91@gmail.com>
2021-12-25 12:46:29 +0900

    test

--- src/App.vue
+++ src/App.vue
@@ -22,5 +22,33 @@
 <style lang="scss">
 @import '~@/assets/scss/_index.scss';
 // 공통 scss는 여기에 정의
+#app{
+        .containerLayout{
+            .content{
+                width: 99%;
+                height: 60px;
+                overflow: hidden;
 
+                .menuNav{
+                    width: 200px;
+                    height: 25px;
+                    display:flex;
+                    justify-content: space-between;
+                    align-items: center;
+                    font-size: $font-12;
+                    color: $color-span;
+
+                    span:last-child{
+                        color:  $color-basic;
+                    }
+                }
+
+                .title{
+                    display: inline-block;
+                    margin-top: 5px;
+                    font-size: $font-24 !important;
+                }
+            }
+        }
+    }
 </style>
--- src/views/healthy/order/order/OrderReception.vue
+++ src/views/healthy/order/order/OrderReception.vue
@@ -1,6 +1,14 @@
 <template xmlns:v-slot="http://www.w3.org/1999/XSL/Transform">
     <v-container fluid class="containerLayout">
         <template>
+            <div class="content">
+                <div class="menuNav">
+                    <span>주문관리</span><v-icon class="pagingArrow">$pagingArrow</v-icon><span>주문관리</span><v-icon class="actionPagingArrow">$actionPagingArrow</v-icon><span>주문접수</span>
+                </div>
+                <div class="title">주문접수</div>
+            </div>
+        </template>
+        <template>
             <!--컨포넌트-->
         </template>
         <template>
