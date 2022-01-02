663d6a84042c3c6c43bb894320df49fa6c230f3a
dongkee.lee <eastflag@gmail.com>
2022-01-02 17:30:11 +0900

    .

--- src/components/common/eachPageComponents/memberComponents/storeModal/CalculationListModal.vue
+++ src/components/common/eachPageComponents/memberComponents/storeModal/CalculationListModal.vue
@@ -8,43 +8,51 @@
         <v-icon>$closeBtn</v-icon>
       </v-card-title>
       <v-card-text>
-        <p class="tableTitle">계정: pro01</p>
+        <p class="tableTitle">계정: store1</p>
         <v-container class="modalTableLayout">
           <table>
             <colgroup>
-              <col width="14%">
-              <col width="22%">
-              <col width="14%">
-              <col width="14%">
-              <col width="16%">
-              <col width="20%">
+              <col width="10%">
+              <col width="12%">
+              <col width="12%">
+              <col width="12%">
+              <col width="12%">
+              <col width="12%">
+              <col width="12%">
+              <col width="*">
             </colgroup>
             <thead>
             <tr>
-              <th><span>일자</span></th>
-              <th><span>정산내역</span></th>
+              <th><span>구분</span></th>
+              <th><span>주문번호</span></th>
               <th><span>정산기준일</span></th>
               <th><span>정산금액</span></th>
               <th><span>지급기준일</span></th>
+              <th><span>지급금액</span></th>
+              <th><span>잔액</span></th>
               <th><span>비고</span></th>
             </tr>
             </thead>
             <tbody>
             <tr>
+              <td><span>입금</span></td>
+              <td><span>OD20211001-12345</span></td>
               <td><span>2021-09-01</span></td>
-              <td><span>상담 적립(상담번호:SC123456789)</span></td>
+              <td><span>5,000,000</span></td>
               <td><span>2021-09-01</span></td>
-              <td><span>+1500원</span></td>
-              <td><span>2021-09-01</span></td>
-              <td></td>
+              <td><span>5,000,000</span></td>
+              <td><span>1,000,000</span></td>
+              <td><span>-</span></td>
             </tr>
             <tr>
+              <td><span>입금</span></td>
+              <td><span>OD20211001-12345</span></td>
               <td><span>2021-09-01</span></td>
-              <td><span>상담 적립(상담번호:SC123456789)</span></td>
+              <td><span>5,000,000</span></td>
               <td><span>2021-09-01</span></td>
-              <td><span>+1500원</span></td>
-              <td><span>2021-09-01</span></td>
-              <td></td>
+              <td><span>5,000,000</span></td>
+              <td><span>1,000,000</span></td>
+              <td>-</td>
             </tr>
             </tbody>
           </table>
