63de86ac9836dae8488abc2ae8aa0e18a62a7d69
hosea-lee <ghtpdkzz@naver.com>
2022-01-02 20:52:14 +0900

    FAQ 목록 추가

--- src/views/healthy/cscenter/FAQ/FAQList.vue
+++ src/views/healthy/cscenter/FAQ/FAQList.vue
@@ -2,15 +2,20 @@
     <v-container fluid class="containerLayout">
         <template>
             <!--컨포넌트-->
+            <cs-faq-search-input/>
         </template>
         <template>
             <!--컨포넌트-->
+            <cs-faq-search-list/>
         </template>
     </v-container>
 </template>
 
 <script>
+import CsFaqSearchInput from '../../../../components/common/eachPageComponents/cscenterComponents/FAQ/CsFaqSearchInput'
+import CsFaqSearchList from '../../../../components/common/eachPageComponents/cscenterComponents/FAQ/CsFaqSearchList'
     export default {
+  components: { CsFaqSearchInput, CsFaqSearchList },
         name: "FAQList"
     }
 </script>
