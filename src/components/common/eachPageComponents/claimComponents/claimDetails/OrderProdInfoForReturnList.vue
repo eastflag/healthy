c8dc4903d9f0d385b10e83ee850da2e14771d15b
qkrwlgh91 <qkrwlgh91@gmail.com>
2022-01-05 07:13:31 +0900

    운송장등록 팝업 개발중

---
+++ src/components/common/eachPageComponents/claimComponents/claimDetails/OrderProdInfoForReturnList.vue
@@ -0,0 +1,168 @@
+<template>
+    <v-main>
+        <div class="title d-flex justify-space-between">
+            <div class="d-flex"><v-icon class="titleIcon">$titleIcon</v-icon><span>주문상품정보</span></div>
+        </div>
+        <table>
+            <colgroup>
+                <col width="8%">
+                <col width="13%">
+                <col width="13%">
+                <col width="7%">
+                <col width="7%">
+                <col width="7%">
+                <col width="7%">
+                <col width="4%">
+                <col width="4%">
+                <col width="12%">
+                <col width="7%">
+                <col width="8%">
+            </colgroup>
+            <thead>
+                <tr style="border-top: 1px solid #E5E5E5">
+                    <th>스토어명</th>
+                    <th>제품이미지</th>
+                    <th>제품명</th>
+                    <th>옵션명</th>
+                    <th>상품금액</th>
+                    <th>할인금액</th>
+                    <th>주문금액</th>
+                    <th>주문수량</th>
+                    <th>교환수량</th>
+                    <th>교환운송장번호</th>
+                    <th>상품확인일자</th>
+                    <th>주문상태</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>종근당</td>
+                    <td valign="middle"><img src="" width="150" height="150" alt="이미지" align="center"/></td>
+                    <td>종근당 슈퍼활력업 파워</td>
+                    <td>-</td>
+                    <td>100,000원</td>
+                    <td>33,000원</td>
+                    <td>70,000원</td>
+                    <td>1</td>
+                    <td>1</td>
+                    <td style="line-height: normal">
+                        택배사 : CJ대한통운
+                        <br />
+                        운송장번호 : 1232435435
+                        <br />
+                        발송일자 : 2021-10-01
+                    </td>
+                    <td>
+                        2021-10-05
+                    </td>
+                    <td>
+                        <div>
+                           교환처리중
+                        </div>
+                        <div>
+                            <basic-btn>교환접수</basic-btn>
+                        </div>
+                        <div>
+                            <basic-btn @click="dialogs.showReturnDirectionModal = true">교환회수지시</basic-btn>
+                        </div>
+                        <div>
+                            <basic-btn >회수입고</basic-btn>
+                        </div>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+        <return-direction-modal :dialogs="dialogs" />
+    </v-main>
+</template>
+
+<script>
+    import BasicBtn from '@/components/common/buttonComponents/BasicBtn'
+    import ReturnDirectionModal from '@/views/healthy/order/claim/Modal/ReturnDirectionModal'
+
+    export default {
+        name: "OrderProdInfoForReturnList",
+        components:{ BasicBtn, ReturnDirectionModal },
+        props: {
+            orderState: {
+                type: String,
+                required: true
+            }
+        },
+        data() {
+            return {
+                dialogs: {
+                    showReturnDirectionModal: false,
+                },
+            }
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+    @import '~@/assets/scss/_index.scss';
+    #app {
+        .title{
+            width: 1612px;
+            margin: 40px auto 0;
+            font-size: $font-16;
+            font-weight: bold;
+
+            .titleIcon{
+                margin-right: 10px;
+            }
+        }
+        table {
+            width: 1612px;
+            max-width: 1612px;
+            margin-top: 20px;
+
+            thead {
+                tr {
+                    th {
+                        text-align: center;
+                        vertical-align: middle;
+                        height: 52px;
+                        font-size: $font-15;
+                        background: $color-table-bg;
+                    }
+                }
+            }
+
+            tbody {
+                tr {
+                    td {
+                        text-align: center;
+                        vertical-align: middle;
+                        min-height: 170px;
+                        font-size: $font-12;
+                        border: 1px solid $color-border;
+                        padding: 5px;
+                        line-height: 38px;
+
+                        &:first-child {
+                            border-left: none;
+                        }
+
+                        &:last-child {
+                            border-right: none;
+                        }
+
+                        img {
+                            text-align: center;
+                            vertical-align: middle;
+                        }
+                    }
+                }
+            }
+        }
+
+        .button{
+            button:first-child{
+                margin-right: 15px;
+            }
+        }
+    }
+</style>
+
+
---
+++ src/views/healthy/order/claim/Modal/ReturnDirectionModal.vue
@@ -0,0 +1,180 @@
+<template xmlns:v-slot="http://www.w3.org/1999/XSL/Transform">
+    <v-dialog v-model="dialogs.showReturnDirectionModal" content-class="rounded-0 elevation-0" width="650px">
+        <v-card class="rounded-0 elevation-0 modalLayout">
+            <div class="and_popup_input">
+                <p class="tableTitle">운송장등록 - (주문번호 : {{orderNum}})</p>
+                <div class="modalBody">
+                    <table>
+                        <colgroup>
+                            <col width="27%">
+                            <col width="70%">
+                        </colgroup>
+                        <tr>
+                            <th>배송업체</th>
+                            <td>
+                                <div class="selectbx d-flex">
+                                    <select id="returnPlace" name="returnPlace">
+                                        <option value="del1">del1</option>
+                                        <option value="del2">del2</option>
+                                    </select>
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th>송장번호</th>
+                            <td>
+                                <input type="text" id="detail" name="detail" placeholder="INPUTBOX">
+                            </td>
+                        </tr>
+                    </table>
+                    <p>아래 선택한 주문상품을 입력하신 배송정보로 배송 처리합니다.</p>
+                    <table>
+                        <colgroup>
+                            <col width="6%">
+                            <col width="34%">
+                            <col width="9%">
+                            <col width="16%">
+                            <col width="16%">
+                            <col width="16%">
+                        </colgroup>
+                        <thead>
+                            <tr>
+                                <th>
+                                    <div class="spot">
+                                        <input class="checkbx" type="checkbox" id="modalchk1" name="list_check"><label for="modalchk1"></label>
+                                    </div>
+                                </th>
+                                <th><span>주문상품</span></th>
+                                <th><span>수량</span></th>
+                                <th><span>배송비</span></th>
+                                <th><span>주상품금액</span></th>
+                                <th><span>주문금액</span></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr></tr>
+                        </tbody>
+                    </table>
+                </div>
+                <div class="button d-flex justify-center">
+                    <delete-btn>닫기</delete-btn>
+                    <action-btn class="upBtn">등록</action-btn>
+                </div>
+            </div>
+        </v-card>
+    </v-dialog>
+</template>
+
+<script>
+    import ActionBtn from '@/components/common/buttonComponents/ActionBtn'
+    import DeleteBtn from '@/components/common/buttonComponents/DeleteBtn'
+
+    export default {
+        name: "ReturnDirection",
+        props: [
+            "dialogs"
+        ],
+        components:{ ActionBtn, DeleteBtn },
+        data() {
+            return {
+                orderNum: 'OD202111001-123456'
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    @import '~@/assets/scss/popUpModal/popUpCommon.scss';
+    #app{
+        .and_popup_input {
+            min-height: 220px;
+            height: 100%;
+        }
+        .modalBody {
+            margin: 25px auto;
+            vertical-align: middle;
+
+            p {
+                font-size: $font-12;
+                margin-top: 20px;
+            }
+
+            table {
+                width: 100%;
+                
+                tr {
+                    height: 38px;
+                    th {
+                        text-align: left;
+                        padding-left: 16px;
+                        vertical-align: middle;
+                        font-size: $font-12;
+                        background: $color-table-bg;
+                        border: 1px solid $color-border;
+                        border-left: none;
+                    }
+
+                    td {
+                        text-align: left;
+                        padding-left: 16px;
+                        vertical-align: middle;
+                        font-size: $font-12;
+                        border: 1px solid $color-border;
+                        border-right: none;
+
+                        .selectbx {
+                            height: inherit;
+                            display: flex;
+                            align-items: center;
+                            select {
+                                width: 55%;
+                                background: url('~@/assets/images/icons/search.svg') no-repeat 205px center;
+
+                                option {
+                                    height: 20px;
+                                    font-size: $font-20;
+                                }
+                            }
+                        }
+                        input {
+                            width: 55%;
+                        }
+                    }
+                }
+            }
+        }
+        .button{
+            margin: 20px auto;
+            button:first-child{
+                margin-right: 15px;
+            }
+        }
+
+        .dm_bx{
+            position: relative;
+            width: 100%;
+            height: inherit;
+            display: flex;
+            align-items: center;
+
+            .datebx {
+                display: inline-block;
+                width: 260px;
+                height: 30px;
+                margin: 0 10px;
+
+                &:first-child {
+                    margin-left: 0;
+                }
+
+                .calen {
+                    background: url('~@/assets/images/icons/calender.svg') no-repeat 226px center;
+                    background-size: 20px auto;
+                    cursor: pointer;
+                    height: 100%;
+                }
+            }
+        }
+        
+    }
+</style>
